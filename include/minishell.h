219350a9d45049b58d7d837dd935168ea8cb9e2c
Fatima ezzahra Naciri ben saghir <fnaciri-@e1r8p8.1337.ma>
2020-12-21 12:33:55 +0100

    include restructed

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fnaciri- <fnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 13:58:35 by fnaciri-          #+#    #+#             */
-/*   Updated: 2020/12/21 12:13:03 by fnaciri-         ###   ########.fr       */
+/*   Updated: 2020/12/21 12:31:21 by fnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,48 +31,65 @@
 int (*builtins[BUILTINS_NUM])(char **);
 char *builtins_str[BUILTINS_NUM];
 
+//prompt
 void prompt();
-char    *ft_getenv(char *name);
+
+//treat line
 void    treat_line(char *line);
-void    print_cmd(t_cmd *cmd);
-void    print_arg(char **arg);
 char    *opr(char *s);
 char    *get_path(char *s);
-void    init_env(char **env);
-void inc_shlvl();
 char    **remove_arg(char **arg, char *s);
-int     is_space(char c);
-void    skip_spaces(char **s);
 int     arg_num(char **arg);
-// builtins
-void    init_builtins();
-int     is_builtins(char *s);
-int    execute_builtins();
-int    cmd_cd(char **arg);
-int		cmd_echo(char **arg);
-int  cmd_pwd(char **arg);
-int cmd_env(char **arg);
-int   cmd_export(char **arg);
-int   cmd_unset(char **arg);
-int    cmd_exit(char **arg);
-void    add_env(char *s);
-char  *extract_env(char *s);
-void    replace_env(char *s);
 char    **ft_argtrim(char **arg, char *set);
-void    remove_env(char *s);
-int exec(t_cmd cmd);
-int     execute(t_cmd *cmd);
-t_red    *treat_red(char *pipeline);
-char  *extract_file(char *s, char *set);
-char    *remove_red(char *cmd);
-void    setup_pipe(t_cmd *cmd);
-void reset_std();
-void open_pipe();
-void close_pipe();
-void    setup_red(t_cmd *cmd);
+
+//print
+void    print_cmd(t_cmd *cmd);
+void    print_arg(char **arg);
+
+//spaces
+int         is_space(char c);
+void        skip_spaces(char **s);
+int         ft_count_space(char *s);
+
+// builtins
+void        init_builtins();
+int         is_builtins(char *s);
+int         execute_builtins();
+int         cmd_cd(char **arg);
+int		    cmd_echo(char **arg);
+int         cmd_pwd(char **arg);
+int         cmd_env(char **arg);
+int         cmd_export(char **arg);
+int         cmd_unset(char **arg);
+int         cmd_exit(char **arg);
+
+//env
+void        init_env(char **env);
+void        inc_shlvl();
+char        *ft_getenv(char *name);
+void        add_env(char *s);
+char        *extract_env(char *s);
+void        replace_env(char *s);
+void        remove_env(char *s);
+
+//execution
+int         exec(t_cmd cmd);
+int         execute(t_cmd *cmd);
+
+//redirection
+t_red       *treat_red(char *pipeline);
+char        *extract_file(char *s, char *set);
+char        *remove_red(char *cmd);
+void        setup_red(t_cmd *cmd);
+
+//pipes
+void        setup_pipe(t_cmd *cmd);
+void        reset_std();
+void        open_pipe();
+void        close_pipe();
+int         ft_redcount(char *cmd, int l);
+
+//signal
 void sig_handler(int signum);
-int     ft_count_space(char *s);
-int     ft_redcount(char *cmd, int l);
-int g_pipe[2];
 
 #endif
--- include/utils.h
+++ include/utils.h
@@ -6,16 +6,16 @@
 /*   By: fnaciri- <fnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 13:58:21 by fnaciri-          #+#    #+#             */
-/*   Updated: 2020/12/13 14:02:35 by fnaciri-         ###   ########.fr       */
+/*   Updated: 2020/12/21 12:31:29 by fnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "struct.h"
 #include "../libft/libft.h"
@@ -31,13 +31,14 @@ void				ft_cmd_add_back(t_cmd **alst, t_cmd *new);
 void                ft_cmd_delone(t_cmd *cmd);
 void                ft_cmd_clear(t_cmd **cmd);
 t_red               *ft_red_new(void *red_type, char *file);
-void	ft_red_add_back(t_red **red, t_red *new);
-t_red		*ft_red_last(t_red *lst);
-t_red	*ft_red_new(void *red_type, char *file);
-char *ft_strndup(const char *s1, size_t n);
-int     ft_countchar(char *s, char c);
-char    *ft_strremove(char *s, char c);
-int     ft_strnchrn(char *s, char *set);
-int     ft_strchrn(char *s, char c);
-int     ft_tab_min(int tab[], int i);
+void	            ft_red_add_back(t_red **red, t_red *new);
+t_red		        *ft_red_last(t_red *lst);
+t_red	            *ft_red_new(void *red_type, char *file);
+char                *ft_strndup(const char *s1, size_t n);
+int                 ft_countchar(char *s, char c);
+char                *ft_strremove(char *s, char c);
+int                 ft_strnchrn(char *s, char *set);
+int                 ft_strchrn(char *s, char c);
+int                 ft_tab_min(int tab[], int i);
+
 #endif
